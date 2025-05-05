bd2749a58c7f8a345b84e87af306c9c535a9f5ea
vincenzo.auricchio <vincenzo.auricchio@check24.de>
2025-05-05 11:29:06 +0200

    Init commit.

---
+++ src/main/java/hm/edu/arc/pi/net/Application.java
@@ -0,0 +1,13 @@
+package hm.edu.arc.pi.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
